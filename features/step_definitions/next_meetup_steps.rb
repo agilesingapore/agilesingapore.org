36e9d43957f93614fce047d80d925c87e967fe2d
Bas Vodde <basv@odd-e.com>
2013-04-29 21:20:27 +0800

    First step definitions for next meet_up

---
+++ features/step_definitions/next_meetup_steps.rb
@@ -0,0 +1,12 @@
+Given(/^I'm have a computer$/) do
+	pending # express the regexp above with the code you wish you had
+end
+
+When(/^I go to the Agile Singapore community page$/) do
+	pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I want to easily see the next meet\-up information$/) do
+	pending # express the regexp above with the code you wish you had
+end
+
